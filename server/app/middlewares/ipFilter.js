4ed473753f3c219bbb4d643ae24475eb69cd25d6
molikai-work <165390418+molikai-work@users.noreply.github.com>
2025-06-06 08:45:05 +0800

    fix(ipFilter): 向Socket对齐中间件的客户端IP获取逻辑

--- server/app/middlewares/ipFilter.js
+++ server/app/middlewares/ipFilter.js
@@ -1,13 +1,17 @@
 // 白名单IP
 const fs = require('fs')
 const path = require('path')
+const { clientIPHeader } = require('../config')
 const { isAllowedIp } = require('../utils/tools')
 
 const htmlPath = path.join(__dirname, '../template/ipForbidden.html')
 const ipForbiddenHtml = fs.readFileSync(htmlPath, 'utf8')
 
 const ipFilter = async (ctx, next) => {
-  if (isAllowedIp(ctx.request.ip)) return await next()
+  // 前者为自定义真实IP请求头或默认兼容nginx反代, 后者为中间件获取的IP地址
+  const requestIP = ctx.request.headers[clientIPHeader] || ctx.request.ip
+
+  if (isAllowedIp(requestIP)) return await next()
   ctx.status = 403
   ctx.body = ipForbiddenHtml
 }
