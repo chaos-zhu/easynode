4b13f1b02d4b1644fcacba1e70d385a659a6b6ee
chaoszhu <ggchaos1022@gmail.com>
2026-05-01 21:48:48 +0800

    feat: 移除获取激活ip

--- server/app/utils/get-plus.js
+++ server/app/utils/get-plus.js
@@ -8,26 +8,11 @@ async function getLicenseInfo(key = '') {
   const { key: plusKey } = await plusDB.findOneAsync({}) || {}
   key = key || plusKey || process.env.PLUS_KEY
   if (!key || key.length < 16) return { success: false, msg: 'Invalid Plus Key' }
-  let ip = ''
-  if (global.serverIp && (Date.now() - global.getServerIpLastTime) / 1000 / 60 < 60) {
-    ip = global.serverIp
-    logger.info('通过缓存获取服务器IP: ', ip)
-  } else {
-    ip = await getLocalNetIP()
-    global.serverIp = ip
-    global.getServerIpLastTime = Date.now()
-    logger.info('通过接口获取服务器IP: ', ip)
-  }
-  if (!ip) {
-    logger.error('😒激活PLUS功能失败: get public ip failed')
-    global.serverIp = ''
-    return { success: false, msg: 'get public ip failed' }
-  }
   try {
     const requestOptions = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ ip, key, version })
+      body: JSON.stringify({ key, version })
     }
 
     const response = await requestWithFailover('/api/licenses/activate', requestOptions)
