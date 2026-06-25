44703b4a8e21c60626f69e662dbeca5ff7dbda48
chaoszhu <chaoszhu@foxmail.com>
2026-06-25 23:19:54 +0800

    fix: disable global multipart body parsing

--- server/app/middlewares/body.js
+++ server/app/middlewares/body.js
@@ -1,12 +1,5 @@
 const { koaBody } = require('koa-body')
-const { uploadDir } = require('../config')
 
 module.exports = koaBody({
-  multipart: true, // 支持 multipart-formdate 的表单
-  formidable: {
-    uploadDir, // 上传目录
-    keepExtensions: true, // 保持文件的后缀
-    multipart: true, // 多文件上传
-    maxFieldsSize: 2 * 1024 * 1024 // 文件上传大小 单位：B
-  }
+  multipart: false
 })
